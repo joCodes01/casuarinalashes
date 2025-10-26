7e2429b156fbcad2aa5d1e6f6473f51b3e9c18e1
joCodes <76087277+joCodes01@users.noreply.github.com>
2025-10-26 17:50:11 +1100

    Fix router and base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/casuarinalashes/',
 })
+
+// Set the correct base in vite.config.js.
+
+// If you are deploying to https://<USERNAME>.github.io/, 
+// or to a custom domain through GitHub Pages (eg. www.example.com), set base to '/'. 
+// Alternatively, you can remove base from the configuration, as it defaults to '/'.
+
+// If you are deploying to https://<USERNAME>.github.io/<REPO>/ (eg. your repository 
+// is at https://github.com/<USERNAME>/<REPO>), then set base to '/<REPO>/'.
+
+// https://vite.dev/guide/static-deploy.html#github-pages
